12814ec1e71ebfbcca04c3c37a3025ef72a3501f
cuberaksi <cuberaksi@gmail.com>
2024-06-07 08:38:16 +0700

    Remove js sticky footer booking now for moment

--- themes/sundara-theme/sundara.js
+++ themes/sundara-theme/sundara.js
@@ -17,29 +17,16 @@
         });
     
         /*Mobile stick footer Product detail*/
-        const $section = $('.product-hero-banner'),
-            $secTarget = $('.yith-wcbk-mobile-fixed-form'),
-            sectionOffset = $section.offset().top;
-        $(window).on('scroll', ()=> {
-            const scrollPosition = $(this).scrollTop();
-            if (scrollPosition <= 100) {
-                $secTarget.removeClass('active');
-            } else {
-                $secTarget.addClass('active');
-            }
-        });
-
-        // $('#sharelink').on('click', function() {
-        //     var text = window.location; 
-        //     var $temp = $('<textarea>');
-        //     $('body').append($temp);
-        //     $temp.val(text).select();
-        //     document.execCommand('copy');
-        //     $temp.remove();
-        //     // Update the button text to "Copied!"
-        //     $(this).text('Copied!');
-        //     // Add the "clicked" class to the button
-        //     $(this).addClass('clicked');
+        // const $section = $('.product-hero-banner'),
+        //     $secTarget = $('.yith-wcbk-mobile-fixed-form'),
+        //     sectionOffset = $section.offset().top;
+        // $(window).on('scroll', ()=> {
+        //     const scrollPosition = $(this).scrollTop();
+        //     if (scrollPosition <= 100) {
+        //         $secTarget.removeClass('active');
+        //     } else {
+        //         $secTarget.addClass('active');
+        //     }
         // });
     });
 })(jQuery)
