708184daa9243d16f1a40286e5878040d62fcb66
aikhacode <aikhacomp@gmail.com>
2024-07-10 13:53:17 +0700

    add preloader

--- themes/sundara-theme/functions.php
+++ themes/sundara-theme/functions.php
@@ -3,5 +3,13 @@
 	 function sundara_theme_enqueue_styles() {
  		  wp_enqueue_style( 'child-style-astra-sundara', get_stylesheet_directory_uri() . '/style.css',[],time() ); 
  		   wp_enqueue_script( 'child-script-astra-sundara', get_stylesheet_directory_uri() . '/sundara.js',['jquery'],time(),true ); 
- 		  } 
+ 		  }
+
+ 	 	add_action( 'wp_body_open', function(){
+ 		?>
+ 		<div class="preloaderz">
+	<img src="https://sundaratravelclub.com/wp-content/uploads/2024/06/logo-sundara-preload.svg" alt="sundara logo">
+</div>
+ 		<?php
+ 	} );  
  ?>
